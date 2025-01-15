5a0f47123d51995ce9c20c25f3f4f3daaffab552
unknown <s24393@pjwstk.edu.pl>
2025-01-15 21:39:43 +0100

    Poetry setup and web scrapping

---
+++ data_harvesting/subtitles/dataset_config.py
@@ -0,0 +1,15 @@
+"""File contains class that gives access to all paths to files."""
+
+
+class DatasetConfig:
+
+    """Class that contains all paths to files.
+
+    This class defines constants for paths to various JSONL files used
+    in the data harvesting pipeline. These include files for processing
+    captions and scraped movie data.
+    """
+
+    TO_WHISPE_R = "../data/movies_to_whisper.jsonl"
+    ALL_SCRAPED_MOVIES = "../data/all_movies.jsonl"
+    NON_GENERATED = "../data/bomba_captions.jsonl"
