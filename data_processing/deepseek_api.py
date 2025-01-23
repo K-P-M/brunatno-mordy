57b88c9a45c5c30de423c3921460d2812594dbff
unknown <s24393@pjwstk.edu.pl>
2025-01-23 21:04:23 +0100

    adds transcription and first preprocess

---
+++ data_processing/deepseek_api.py
@@ -0,0 +1,13 @@
+# import openai
+# import json
+#
+#
+# class DeepSeekDataProcessing:
+#     def __init__(self, read_file:str,api_key):
+#         self.read_file = read_file
+#         self.api_key=api_key
+#         self.responses=[]
+#     def call_api(self):
+#
+
+
