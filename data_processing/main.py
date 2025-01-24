40f18280a4a2fcbbda21cb9261c381155f31c973
Paweł Pepliński <p.peplinski7@gmail.com>
2025-01-24 21:05:10 +0100

    feat: add code for preparing inputs for text

---
+++ data_processing/main.py
@@ -0,0 +1,28 @@
+from data_processing.deepseek_api import DeepSeekInputCreator
+import os
+import json
+
+
+def load_chunks():
+    chunks = []
+
+    with open('../data_harvesting/data_cleaning/chunked_2.jsonl', 'r', encoding='utf-8') as file:
+        for line in file:
+            data = json.loads(line)
+            chunks.append(data['chunks'])
+
+    return chunks
+
+
+if __name__ == "__main__":
+    api_key = os.environ.get("API_KEY")
+
+    chunks = load_chunks()
+
+    creator = DeepSeekInputCreator(api_key)
+    descriptions = creator.create_descriptions(chunks)
+
+    with open('train_datasets/descriptions.jsonl', 'w', encoding='utf-8') as file:
+        for description in descriptions:
+            json.dump(description, file, ensure_ascii=False)
+            file.write('\n')
