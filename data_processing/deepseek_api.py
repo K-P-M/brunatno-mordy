e5568e089751bb3160941843ded99f586fc6ee19
Paweł Pepliński <p.peplinski7@gmail.com>
2025-01-24 21:05:10 +0100

    feat: add code for preparing inputs for text

--- data_processing/deepseek_api.py
+++ data_processing/deepseek_api.py
@@ -1,13 +1,54 @@
-import openai
 import json
+from openai import OpenAI
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from tqdm import tqdm
 
 
-# class DeepSeekDataProcessing:
-#     def __init__(self, read_file:str,api_key):
-#         self.read_file = read_file
-#         self.api_key=api_key
-#         self.responses=[]
-#     def call_api(self):
-#
+class DeepSeekInputCreator:
+    def __init__(self, api_key: str):
+        self.client = OpenAI(
+            api_key=api_key,
+            base_url="https://api.deepseek.com",
+        )
 
+        self.system_prompt = """
+        Stwórz opisy dla podanego tekstu. Opis umieść w polu "user", a niezmieniony tekst w polu "assistant".
+        
+        EXAMPLE USER INPUT:
+        Nazywam się Cezary Baryka, od dwudziestu minut jestem właścicielem tego oto szklanego domu. Powoli zaczynam żałować zakupu. W nocy pizga, w dzień parówa. Zero wentylacji i brak kanalizacji robią swoje.
+        
+        EXAMPLE JSON OUTPUT:
+        {
+            "user": "Opowiedz jak jak to jest mieszkać w szklanym domu",
+            "assistant": "Nazywam się Cezary Baryka, od dwudziestu minut jestem właścicielem tego oto szklanego domu. Powoli zaczynam żałować zakupu. W nocy pizga, w dzień parówa. Zero wentylacji i brak kanalizacji robią swoje."
+        }
+        """
 
+    def create_descriptions(self, chunks: list[str]) -> list[dict[str, str]]:
+        with ThreadPoolExecutor() as executor:
+            futures = [executor.submit(self._create_description, chunk) for chunk in chunks]
+
+            descriptions = []
+            with tqdm(total=len(chunks), desc="Generating Descriptions", unit="chunk") as pbar:
+                for future in as_completed(futures):
+                    descriptions.append(future.result())
+                    pbar.update(1)
+
+        return descriptions
+
+    def _create_description(self, chunk: str) -> dict:
+        messages = [
+            {"role": "system", "content": self.system_prompt},
+            {"role": "user", "content": chunk}
+        ]
+
+        response = self.client.chat.completions.create(
+            model="deepseek-chat",
+            messages=messages,
+            response_format={
+                'type': 'json_object'
+            },
+            temperature=1.5,
+        )
+
+        return json.loads(response.choices[0].message.content)
